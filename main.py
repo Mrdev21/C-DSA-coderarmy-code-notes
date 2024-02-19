48d6153048056f35f2a23ba680f3a610d1615a9a
Himanshu Chaudhari <148252440+Mrdev21@users.noreply.github.com>
2024-02-19 11:27:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
+#import os
 
-if(not os.path.exists("data")):
+#if(not os.path.exists("data")):
   
-  os.mkdir("data")
+ # os.mkdir("data")
 
-for i in range(8, 180):
-  os.mkdir(f"data/L-{i+1}")
+#for i in range(8, 180):
+ # os.mkdir(f"data/L-{i+1}")
