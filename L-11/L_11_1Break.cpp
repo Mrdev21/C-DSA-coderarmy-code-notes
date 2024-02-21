11de3efefc332cb0445d6866a56ba0683fb4c801
Himanshu Chaudhari <148252440+Mrdev21@users.noreply.github.com>
2024-02-22 00:22:43 +0530

    Add files via upload

---
+++ L-11/L_11_1Break.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    //Break
+
+    for(int i=1; i<=10; i++)
+    {
+        if(i==4)
+        break;
+
+        cout<<i<<" ";
+        
+    }
+}
---
+++ L-11/L_11_2continue.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    //Continue
+
+    for(int i=1; i<=10; i++)
+    {
+        if(i==4)
+        continue;
+
+        cout<<i<<" ";
+        
+    }
+}
