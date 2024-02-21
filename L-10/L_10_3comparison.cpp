b174c17d900b97c8c69476c397987d11daad5d6c
Himanshu Chaudhari <148252440+Mrdev21@users.noreply.github.com>
2024-02-21 20:05:31 +0530

    Add files via upload

---
+++ L-10/L_10_3comparison.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //comparison operator
+    // 1 or 0
+
+    int a, b;
+    cout<<"Enter the value of a: ";
+
+    cin>>a;
+    cout<<"Enter your value: ";
+    cin>>b;
+
+    // ==
+    //if(a==b)
+    //cout<<"yes";
+    //else
+    //cout<<"no";
+
+
+    /*if(a<b)
+    cout<<"yes";
+    else
+    cout<<"no";
+    */
+    
+    /*if(a>b)
+    cout<<"yes";
+    else
+    cout<<"no";
+    */
+    
+    /*if(a<=b)
+    cout<<"yes";
+    else
+    cout<<"no";
+
+    */
+
+
+    /*if(a>=b)
+    cout<<"yes";
+    else
+    cout<<"no;*/
+
+
+    if(a!=b)
+    cout<<"yes";
+    else
+    cout<<"no";
+
+
+
+
+}
