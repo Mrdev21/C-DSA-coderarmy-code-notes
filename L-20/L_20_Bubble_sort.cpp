4cf13997d29b1d4552f0e70dd8defd1395be522a
Himanshu Chaudhari <148252440+Mrdev21@users.noreply.github.com>
2024-02-25 16:48:55 +0530

    Add files via upload

---
+++ L-20/L_20_Bubble_sort.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[1000];
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    cout<<"Enter the elelment in array: ";
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+
+    }
+
+    for(int i=n-2; i>=0; i--)
+    {
+        bool swapped = 0;
+        for(int j = 0; j<=i; j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swapped=1;
+                swap(arr[j],arr[j+1]);
+
+            }
+
+        }
+
+        if(swapped==0)
+        break;
+    }
+
+    for(int i=0; i<n; i++)
+    cout<<arr[i]<<" ";
+
+
+}
